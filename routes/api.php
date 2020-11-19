fe98a4753949ea00882fdc14726f40487ccaf1d4
Where.LIU <liuzhaowei55@gmail.com>
2020-11-19 22:57:12 +0800

    feat: router

--- routes/api.php
+++ routes/api.php
@@ -1,32 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\HealthController;
 use App\Http\Controllers\RandomController;
+use Illuminate\Support\Facades\Route;
 
-Route::post('user/register', 'Api\AuthController@register');
-Route::post('user/login', 'Api\AuthController@login');
+Route::post('user/register', [AuthController::class, 'register']);
+Route::post('user/login', [AuthController::class, 'login']);
 Route::middleware([App\Http\Middleware\CheckApiToken::class])->group(
     function () {
-        Route::get('user/profile', 'Api\UserController@profile');
+        Route::get('user/profile', [UserController::class, 'profile']);
         // post
-        Route::post('post/save', 'Api\PostController@save');
+        Route::post('post/save', [PostController::class, 'save']);
     }
 );
-
-
-
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::get('health', [HealthController::class, 'action']);
 Route::get('random/string', [RandomController::class, 'string']);
