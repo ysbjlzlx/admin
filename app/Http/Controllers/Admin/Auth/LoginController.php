88878b8391923dc95d09a73e672bb7587851cfba
liuzhaowei <liuzhaowei55@gmail.com>
2020-11-25 02:06:04 +0800

    feat(migrations): admin

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
     public function redirectLoginView()
     {
         return view('admin.auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $rules = [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+            'totp_key' => 'required|number|digits:6',
+        ];
+        $request->validate($rules);
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Admin\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('login', [LoginController::class, 'redirectLoginView']);
+Route::post('login', [LoginController::class, 'login']);
