dbd9113e5cf98358cfa6f875f95e2cfc281deead
liuzhaowei <liuzhaowei55@gmail.com>
2020-11-24 20:28:51 +0800

    feat: db

--- database/migrations/0000_00_00_000001_create_user_table.php
+++ database/migrations/0000_00_00_000001_create_user_table.php
@@ -15,14 +15,18 @@ class CreateUserTable extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->id()->comment('主键');
-            $table->string('email')->default('')->comment('Email')->unique('uk_email');
+            $table->string('email')->default('')->comment('Email');
             $table->string('password')->nullable()->comment('密码');
             $table->string('nickname')->nullable()->comment('昵称');
             $table->string('avatar')->nullable()->comment('头像');
+            $table->string('totp_secret')->nullable()->comment('TOTP Secret');
             $table->dateTime('email_verified_at')->nullable()->comment('邮箱验证时间');
             $table->rememberToken();
-            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate()->comment('更新时间')->index('idx_updated_at');
-            $table->timestamp('created_at')->useCurrent()->comment('创建时间')->index('idx_created_at');
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate()->comment('更新时间');
+            $table->timestamp('created_at')->useCurrent()->comment('创建时间');
+            $table->unique('email', 'uk_email');
+            $table->index('updated_at', 'idx_updated_at');
+            $table->index('created_at', 'idx_created_at');
         });
     }
 
